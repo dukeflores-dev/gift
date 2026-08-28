7e7bf0c339a3aa25790eff34ba05e46958a43497
dukeflores-dev <dukejasperlanceflores@gmail.com>
2026-08-29 00:36:53 +0800

    commit

--- script.js
+++ script.js
@@ -6,45 +6,51 @@ const heartWrap = document.querySelector('.heart-wrap');
 
 const messages = [
   {
-    title: '"For God so loved the world..."',
-    text: '“For God so loved the world that He gave His one and only Son, that whoever believes in Him shall not perish but have eternal life.” — John 3:16'
+    title: '"Proverbs 3:6"',
+    text: '“God is guiding your next step.” — Proverbs 3:6'
   },
   {
-    title: '"I can do all things..."',
+    title: '"Philippians 4:13"',
     text: '“I can do all things through Christ who strengthens me.” — Philippians 4:13'
   },
   {
-    title: '"Trust in the Lord..."',
+    title: '"Proverbs 3:5"',
     text: '“Trust in the Lord with all your heart and lean not on your own understanding.” — Proverbs 3:5'
   },
   {
-    title: '"The Lord is my shepherd..."',
+    title: '"Psalm 23:1"',
     text: '“The Lord is my shepherd; I shall not want.” — Psalm 23:1'
   },
   {
-    title: '"Be strong and courageous..."',
+    title: '"Joshua 1:9"',
     text: '“Be strong and courageous. Do not be afraid; do not be discouraged, for the Lord your God will be with you wherever you go.” — Joshua 1:9'
   },
   {
-    title: '"God is our refuge..."',
+    title: '"Psalm 46:1"',
     text: '“God is our refuge and strength, an ever-present help in trouble.” — Psalm 46:1'
   },
   {
-    title: '"With God all things are possible..."',
+    title: '"Matthew 19:26"',
     text: '“With God all things are possible.” — Matthew 19:26'
   },
   {
-    title: '"Cast all your anxiety..."',
+    title: '"1 Peter 5:7"',
     text: '“Cast all your anxiety on Him because He cares for you.” — 1 Peter 5:7'
   },
   {
-    title: '"The joy of the Lord..."',
+    title: '"Nehemiah 8:10"',
     text: '“The joy of the Lord is your strength.” — Nehemiah 8:10'
   },
   {
-    title: '"Let all that you do be done in love..."',
+    title: '"1 Corinthians 16:14"',
     text: '“Let all that you do be done in love.” — 1 Corinthians 16:14'
   }
+  
+  {
+     title: ' "Mark 11:24"',
+    text: ' "Nothing is more powerful than prayer, nothing is stronger than faith, and nothing is greater than God." — Mark 11:24'
+  }
+   
 ];
 
 let index = 0;
